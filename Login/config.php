696898dd910e0af77be0b6b41dc3e9570fc024cb
SarinJohn <100458346+SarinJohn@users.noreply.github.com>
2022-02-27 20:49:27 +0530

    Add files via upload

---
+++ Login/config.php
@@ -0,0 +1,11 @@
+<?php
+
+  return array(
+    'admin_user' => 'administrator',
+    'admin_password' => 'admin@123',
+    'admin_email' => 'admin@agrocul.com',
+    'admin_phone' => '9898989898',
+    'admin_address' => 'Kerala, India'
+  );
+
+?>
